094bd86c9b7a85fd3d7273018a942c01b73f2c55
CristianGG <cristancho1107@gmail.com>
2024-02-08 16:28:42 -0500

    ted

--- Main.java
+++ Main.java
@@ -1,13 +1,10 @@
-import javax.swing.JOptionPane;
+
 public class Main{
 
     // Método principal
     public static void main(String[] args) {
         // Código principal del programa
         System.out.println("¡Hola, mundo!");
-        JOptionPane.showMessageDialog(null, "Sapo");
-        JOptionPane.showInputDialog(null, "szs");
-        JOptionPane.showInputDialog(null, "szs");
-        JOptionPane.showInputDialog(null, "szs");
+
     }
 }
